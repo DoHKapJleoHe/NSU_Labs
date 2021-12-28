e2df958ea76dfb9666ed3e69148dfb343900d82a
DoHKapJleoHe <91648774+DoHKapJleoHe@users.noreply.github.com>
2021-12-28 23:28:05 +0700

    Update Lab0.c

--- C/Lab0/Lab0.c
+++ C/Lab0/Lab0.c
@@ -1,144 +1,171 @@
 
-#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-int char_to_dig(char c)
-{
-	if (c >= '2' && c <= '9')
-		return c - '0';
-	else if (c >= 'A' && c <= 'F')
-		return c - 'A' + 10;
-	else if (c >= 'a' && c <= 'f')
-		return c - 'a' + 10;
+#include <stdlib.h>
+
+int char_to_dig(char c) {
+	
+	if (c >= 'a' && c <= 'z')
+		c += 'A' - 'a';
+
+	return (c >= 'A' && c <= 'Z') ? (10 + c - 'A') : (c - '0');
 }
-int dig_to_char(int c)
-{
-	if (c >= 2 && c <= 9)
-		return c + '0';
-	else if (c >= 'A' && c <= 'F')
-		return 'A' + c - 10;
-	else if (c >= 'a' && c <= 'f')
-		return 'a' + c - 10;
+
+char dig_to_char(int n) {
+
+	return (n >= 0 && n <= 9) ? ('0' + n) : ('A' - 10 + n);
 }
-double translate_to_10_drobnoe(int num_sys, char* number)  // эта функция выполняется если число поданное на вход - дробное
-{
-	int integer_part = 0;
-	double rez = 0;
-	int c = 0, i = 0;
-	for (i = 0; (c = *number) != '.'; ++i)
+
+	long double translate_to_10_drobnoe(int num_sys, char* number)
 	{
-		integer_part += (integer_part * num_sys) + char_to_dig(c); // разобраться с этим. странный способ перевода
+		long double sum = 0;
+		long long int integer_part = 0;
+		int i = 0;
+		char c = 0;
+
+
+		for (i = 0; (c = *number) != '.'; ++i) {
+			integer_part = (integer_part * num_sys) + char_to_dig(c);
+			++number;
+		}
+
+
 		++number;
+
+
+		for (i = 0; integer_part > 0; ++i) {
+			sum += (integer_part % num_sys) * pow(num_sys, i);
+			integer_part /= num_sys;
+		}
+
+
+		for (i = -1; (c = *number) != '\0'; --i) {
+			sum += char_to_dig(c) * pow(num_sys, i);
+			++number;
+		}
+		return sum;
 	}
-	number++; // пропуск точки
-	for (i = 0; integer_part > 0; i++)
-	{
-		rez += (integer_part % num_sys) * pow(num_sys, i);
-		integer_part /= num_sys;
-	}
-	for (i = -1; (c = *number) != '/0'; --i)
-	{
-		rez += char_to_dig(c) * pow(num_sys, i);
-	}
-	return rez;
-}
-double translate_to_10_celoe(int num_sys, char* number)  // эта функция выполняется если число поданное на вход - целое
-{
+
+double translate_to_10_celoe(int num_sys, char* number) {
+	long double sum = 0;
 	long long int integer_part = 0;
-	double rez = 0;
-	int c = 0, i = 0;
-	for (i = 0; (c = *number) != '.';++i)
-	{
-		integer_part += (integer_part * num_sys) + char_to_dig(c); // разобраться с этим. странный способ перевода
+	int i = 0;
+	char c = 0;
+	
+	for (i = 0; (c = *number) != '\0'; ++i) {
+		integer_part = (integer_part * num_sys) + char_to_dig(c);
 		++number;
 	}
-	for (i = 0; integer_part > 0; i++)
-	{
-		rez += (integer_part % num_sys) * pow(num_sys, i);
+
+	for (i = 0; integer_part > 0; ++i) {
+		sum += (integer_part % num_sys) * pow(num_sys, i);
 		integer_part /= num_sys;
 	}
-	return rez;
+	return sum;
 }
-void translate(int number, int num_sys)
-{
+
+void translate(long double number, int num_sys) {
 	int i = 0;
+	char integer_result_end[100], fract_result_end[100];
 	long long int integer_part = (long long int)number;
 	long double fract_part = number - (long long int)number;
-	char integer_part_end[100];
-	char fract_part_end[100];
-	while (integer_part)
-	{
-		integer_part_end[i++] = dig_to_char(integer_part % num_sys);
+
+	do {
+		integer_result_end[i++] = dig_to_char(integer_part % num_sys);
 		integer_part /= num_sys;
+	} while (integer_part);
+	integer_result_end[i] = '\0';
+	int len = i;
+	for (i = 0; i < len / 2; ++i) {
+		int temp = integer_result_end[i];
+		integer_result_end[i] = integer_result_end[len - i - 1];
+		integer_result_end[len - i - 1] = temp;
 	}
-	int len = i / 2;
-	for (i = 0; i < len / 2; ++i)
-	{
-		int temp = integer_part_end[i];
-		integer_part_end[i] = integer_part_end[len - i - 1];
-		integer_part_end[len - i - 1] = temp;
-	}
+
 	i = 0;
-	while (fract_part > 0 && i < 12)
-	{
+	while (fract_part > 0 && i < 12) {
 		fract_part *= num_sys;
-		integer_part = (int)fract_part;
-		fract_part -= (int)fract_part;
-		fract_part_end[i++] = dig_to_char(integer_part);
+		integer_part = (long long int)fract_part;
+		fract_part -= (long long int)fract_part;
+		fract_result_end[i++] = dig_to_char(integer_part);
 	}
+
+	fract_result_end[i] = '\0';
+
 	if (i == 0)
-		printf("%s.0\n", integer_part_end);
+		printf("%s.0\n", integer_result_end);
 	else
-		printf("%s.%s\n", integer_part_end, fract_part_end);
-	return 0;
+		printf("%s.%s\n", integer_result_end, fract_result_end);
 }
-int main()
-{
-	int num_sys1;
-	int num_sys2;
+
+int main() {
+
+	int num_sys1 = 0;
+	int num_sys2 = 0;
 	char number[100];
+	long double dec = 0;
 	int fract_part = 0;
-	long double summ = 0;
-	scanf_s("%d %d", &num_sys1, &num_sys2);
-	
-	gets(number);// поробовать использовать fgets или fgetws
-	if (num_sys1 < 2 || num_sys1 > 16 || num_sys2 < 2 || num_sys2 > 16)
+	if (1 != scanf("%d \n", &num_sys1))
 	{
-		printf("Bad input\n");
-		exit(0);
+		printf("Bad input!");
+		return 0;
 	}
+	if (1 != scanf("%d \n", &num_sys2))
+	{
+		printf("Bad input!");
+		return 0;
+	}
+	if(!fgets(number, 100, stdin))
+	{
+	    printf("bad input\n");
+	}
+
+	if ((num_sys1 < 2 || num_sys1 > 16) || (num_sys2 < 2 || num_sys2 > 16)) 
+	{
+		printf("bad input\n");
+		return 0;
+	}
+
 	char* p = number;
 	if (*p == '.')
 	{
-		printf("Bad input\n");
-		exit(0);
+		printf("bad input\n");
+		return 0;
 	}
-
-	while (*p)
-	{
-		if ((char_to_dig(*p) > num_sys1 - 1) && (*p != '.'))
+	while (*p) {
+		if ((char_to_dig(*p) > num_sys1 - 1) && (*p != '.')) 
 		{
 			printf("bad input\n");
 			return 0;
 		}
-		if (*p == '.')
+		if (*p == '.') 
 		{
 			fract_part += 1;
 		}
 		++p;
 	}
-	if (fract_part > 1)
+	--p;
+	if (*p == '.') 
 	{
-		printf("Bad input\n");
-		exit(0);
+		printf("bad input\n");
+		return 0;
 	}
 
-	// Translating //
-	if (fract_part == 1)
-		summ = translate_to_10_drobnoe(num_sys1, number);
+	if (fract_part > 1) 
+	{
+		printf("bad input\n");
+		return 0;
+	}
+
+	if (fract_part)
+		dec = translate_to_10_drobnoe(num_sys1, number);
 	else
-		summ = translate_to_10_celoe(num_sys1, number);
-	translate(summ, num_sys2);
+		dec = translate_to_10_celoe(num_sys1, number);
+
+	translate(dec, num_sys2);
+
 	return 0;
 }
+
