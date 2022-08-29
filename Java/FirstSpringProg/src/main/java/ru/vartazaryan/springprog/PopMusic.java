8bf5f2741d6137405bde2028aab1248e4e7ba9dc
DoHKapJleoHe <eduardvartazaryan1@gmail.com>
2022-08-29 22:22:37 +0700

    heh

---
+++ Java/FirstSpringProg/src/main/java/ru/vartazaryan/springprog/PopMusic.java
@@ -0,0 +1,9 @@
+package ru.vartazaryan.springprog;
+
+public class PopMusic implements Music
+{
+    @Override
+    public String getSong() {
+        return "They Don't Really Care About As";
+    }
+}
