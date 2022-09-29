634d1944c78875702f88d91d0a5ec909fc01a305
eduar <eduardvartazaryan1@gmail.com>
2022-09-30 00:20:28 +0700

    29.09.2022

--- Java/Transmiter/src/com/company/Client.java
+++ Java/Transmiter/src/com/company/Client.java
@@ -2,5 +2,16 @@ package com.company;
 
 public class Client
 {
+    private String path;
+
+    public Client(String path)
+    {
+        this.path = path;
+    }
+
+    public void run()
+    {
+
+    }
 
 }
--- Java/Transmiter/src/com/company/Server.java
+++ Java/Transmiter/src/com/company/Server.java
@@ -1,4 +1,33 @@
 package com.company;
 
-public class Server {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server extends IOException
+{
+    private int port;
+
+    public Server(int port)
+    {
+        this.port = port;
+    }
+
+    public void run() throws IOException
+    {
+        ServerSocket serverSocket = new ServerSocket(port); // <1000 are sys ports
+
+        Socket clientSocket = serverSocket.accept(); // waiting for client
+
+        Scanner in = new Scanner(clientSocket.getInputStream());
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+
+
+
+
+        clientSocket.close();
+        serverSocket.close();
+    }
 }
